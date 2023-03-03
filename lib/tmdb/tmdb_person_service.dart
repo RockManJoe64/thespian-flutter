05789f75cb8eeee47e807c3aefbc032fdee698a7
Jose <jose.e.chavez@gmail.com>
2023-03-02 23:45:22 -0600

    Moved TMDB services and models to tmdb directory

--- lib/tmdb/tmdb_person_service.dart
+++ lib/tmdb/tmdb_person_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
-import 'package:thespian/src/models/tmdb/tmdb_person.dart';
+import 'package:thespian/tmdb/tmdb_person.dart';
 
 class TMDBPersonService {
   Future<List<TMDBPerson>> fetchPopularPeople() async {
