eeb90ae2215addb0153f8c38936afc61ff5db022
Jose <jose.e.chavez@gmail.com>
2023-03-02 23:40:08 -0600

    Created TMDBPersonService to fetchPopularPeople

---
+++ lib/src/services/tmdb_person_service.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:thespian/src/models/tmdb/tmdb_person.dart';
+
+class TMDBPersonService {
+  Future<List<TMDBPerson>> fetchPopularPeople() async {
+    final apiKey = dotenv.env['TMDB_API_KEY'];
+    final response = await http.get(Uri.parse('https://api.themoviedb.org/3/person/popular?api_key=$apiKey'));
+
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      final List<dynamic> data = jsonData['results'];
+      final people = data.map((item) => TMDBPerson.fromJson(item)).toList();
+      return people;
+    } else {
+      throw Exception('Failed to load popular actors');
+    }
+  }
+}
