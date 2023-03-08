4618583315816cd7e566a0345a139e6a99382cf2
Jose <jose.e.chavez@gmail.com>
2023-03-07 23:06:35 -0600

    Refactored PopularActors widgets to use a controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
 
 import 'package:thespian/features/popular_actors/popular_actors_grid_view.dart';
-import 'package:thespian/models/transformers.dart';
 import 'package:thespian/services/service_locator.dart';
-import 'package:thespian/tmdb/tmdb_configuration_service.dart';
-import 'package:thespian/tmdb/tmdb_person_service.dart';
 
-import 'models/actor.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
@@ -20,7 +17,7 @@ class ThespianApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp( // GetMaterialApp is a wrapper around MaterialApp
       title: 'Thespian',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -40,37 +37,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // TODO move this state logic to the PopularActorsGridView widget
-  final TMDBConfigurationService _tmdbConfigurationService = getIt<TMDBConfigurationService>();
-  final TMDBPersonService _tmdbPersonService = getIt<TMDBPersonService>();
-  final ScrollController _scrollController = ScrollController();
-  int _currentPage = 1;
-  List<Actor> actors = [];
-
   @override
   void initState() {
     super.initState();
-    _fetchPopularActors();
-    _scrollController.addListener(_scrollListener);
   }
 
-  void _fetchPopularActors({int page = 1}) async {
-    final config = await _tmdbConfigurationService.fetchImageConfiguration();
-    final response = await _tmdbPersonService.fetchPopularPeople(page: page);
+  void searchActors() {
 
-    setState(() {
-      final converted = convertToActors(config, response);
-      converted.sort((a, b) => b.popularity.compareTo(a.popularity));
-      actors.addAll(converted);
-    });
-  }
-
-  void _scrollListener() {
-    if (_scrollController.offset >= _scrollController.position.maxScrollExtent &&
-        !_scrollController.position.outOfRange) {
-      _currentPage++;
-      _fetchPopularActors(page: _currentPage);
-    }
   }
 
   @override
@@ -80,10 +53,10 @@ class _HomePageState extends State<HomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: PopularActorsGridView(actors: actors, scrollController: _scrollController)
+        child: PopularActorsGridView()
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _fetchPopularActors,
+        onPressed: searchActors,
         tooltip: 'Search',
         child: const Icon(Icons.search),
       ),
