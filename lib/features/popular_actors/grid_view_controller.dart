14d26ea321d895f85dfebd37bf4875b41b8c7a63
Jose <jose.e.chavez@gmail.com>
2023-05-01 22:23:28 -0700

    Renamed file from transformers to popular_person_transformers

--- lib/features/popular_actors/grid_view_controller.dart
+++ lib/features/popular_actors/grid_view_controller.dart
@@ -1,7 +1,7 @@
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
-import 'package:thespian/models/transformers.dart';
+import 'package:thespian/models/popular_person_transformers.dart';
 import 'package:thespian/services/service_locator.dart';
 import 'package:thespian/tmdb/tmdb_configuration_service.dart';
 import 'package:thespian/tmdb/tmdb_image_configuration.dart';
