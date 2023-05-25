9e073cbf6c337509ce6a42586ffdb196109e1456
Jose <jose.e.chavez@gmail.com>
2023-05-24 22:43:10 -0700

    Created search result model for TMDB

---
+++ lib/tmdb/tmdb_search_result.dart
@@ -0,0 +1,36 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:thespian/tmdb/tmdb_known_for.dart';
+
+part 'tmdb_search_result.freezed.dart';
+part 'tmdb_search_result.g.dart';
+
+@freezed
+class TMDBSearchResult with _$TMDBSearchResult {
+  @JsonSerializable(fieldRename: FieldRename.snake)
+  const factory TMDBSearchResult({
+    bool? adult,
+    String? backdropPath,
+    String? firstAirDate,
+    int? gender,
+    List<int>? genreIds,
+    int? id,
+    List<TmdbKnownFor>? knownFor,
+    String? knownForDepartment,
+    String? mediaType,
+    String? name,
+    String? originalLanguage,
+    String? originalTitle,
+    List<String>? originCountry,
+    String? overview,
+    String? posterPath,
+    double? popularity,
+    String? profilePath,
+    String? releaseDate,
+    bool? video,
+    double? voteAverage,
+    int? voteCount,
+  }) = _TMDBSearchResult;
+
+  factory TMDBSearchResult.fromJson(Map<String, dynamic> json) =>
+      _$TMDBSearchResultFromJson(json);
+}
